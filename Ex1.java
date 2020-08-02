b4b9fdb3171b10c19bdaa3fc9d3b42fddfda70f8
keerthi2222 <66769698+keerthi2222@users.noreply.github.com>
2020-08-02 17:37:41 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,27 @@
+package list;
+import java.util.ArrayList;
+
+public class Ex1 
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> al=new ArrayList<>();
+		al.add("January");
+		al.add("Feburary");
+		al.add("March");
+		al.add("April");
+		al.add("May");
+		al.add("June");
+		al.add("July");
+		al.add("August");
+		al.add("September");
+		al.add("October");
+		al.add("November");
+		al.add("December");
+	
+		
+			System.out.println(al);
+		
+	}
+
+}
